1a4aca4e34a61534d166149da30935de952e96bd
BUTCHER16 <ravidossalbert@gmail.com>
2024-08-02 17:27:10 +0530

    Final Commit for the day

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+    'author' : 'Jane Doe',
+    'title' : 'Blog Post 1',
+    'content' : 'This is the First Post',
+    'date_posted' : 'Aug 28, 2022'
+    },
+    {
+    'author' : 'Albert Ravidoss',
+    'title' : 'Blog Post 2',
+    'content' : 'This is the Second Post',
+    'date_posted' : 'Aug 28, 2022'
+    }
+    
+]
+
 def home(request):
-    return HttpResponse('<h1> Blog Home </h1>')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1> Blog About </h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
